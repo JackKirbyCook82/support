c4a5e6386530968597d435f82fec327050693192
JackKirbyCook82 <jackcookchee@yahoo.com>
2023-10-26 03:28:31 -0500

    Updated 8/7/2023

--- calculations.py
+++ calculations.py
@@ -11,12 +11,16 @@ from abc import ABC, ABCMeta
 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
-__all__ = ["Calculation"]
+__all__ = ["Calculation", "equation"]
 __copyright__ = "Copyright 2023, Jack Kirby Cook"
 __license__ = ""
 
 
 LOGGER = logging.getLogger(__name__)
+
+
+def equation():
+    pass
 
 
 class CalculationMeta(ABCMeta):
