5879559f8b9bf2711c7749741f9d8246a08a241a
JackKirbyCook82 <jackcookchee@yahoo.com>
2023-11-01 16:31:51 -0500

    Updated 8/7/2023

--- calculations.py
+++ calculations.py
@@ -15,7 +15,7 @@ from support.mixins import Node
 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
-__all__ = ["Calculation", "Equation", "Feed", "equation"]
+__all__ = ["Calculation", "Equation", "Feed", "equation", "source"]
 __copyright__ = "Copyright 2023, Jack Kirby Cook"
 __license__ = ""
 
@@ -23,12 +23,16 @@ __license__ = ""
 LOGGER = logging.getLogger(__name__)
 
 
-def equation():
+def equation(name, datatype, *args, domain, function, **kwargs):
+    assert isinstance(domain, tuple) and callable(function)
+
+
+def source(name, variables, *args, **kwargs):
+    assert isinstance(variables, dict)
+
+
+class Source(object):
     pass
-
-
-Locator = ntuple("Locator", "key name")
-Position = ntuple("Position", "source variable")
 
 
 class Stage(Node):
@@ -49,15 +53,27 @@ class Equation(Stage):
 class CalculationMeta(ABCMeta):
     def __new__(mcs, name, bases, attrs, *args, **kwargs):
         equations = [key for key, value in attrs.items() if inspect.isclass(value) and issubclass(value, Equation)]
-        attrs = {key: value for key, value in attrs.items() if key not in equations}
+        sources = [key for key, value in attrs.items() if isinstance(value, Source)]
+        exclude = equations + sources
+        attrs = {key: value for key, value in attrs.items() if key not in exclude}
         try:
             cls = super(CalculationMeta, mcs).__new__(mcs, name, bases, attrs, *args, **kwargs)
         except TypeError:
             cls = super(CalculationMeta, mcs).__new__(mcs, name, bases, attrs)
         return cls
 
-    def __init__(cls, *args, sources={}, variables={}, constants={}, **kwargs):
-        pass
+    def __init__(cls, name, bases, attrs, *args, variables={}, **kwargs):
+        assert isinstance(variables, dict)
+        arguments = {index: value for index, value in getattr(cls, "__arguments__", {}).items()}
+        parameters = {key: value for key, value in getattr(cls, "__parameters__", {}).items()}
+        arguments[0] = {variable: name for variable, name in arguments.get(0, {}).items()}
+        arguments[0].update(variables)
+        sources = {key: value for key, value in attrs.items() if isinstance(value, Source)}
+        for key, value in sources.items():
+            parameters[key] = {variable: name for variable, name in parameters.get(key, {}).items()}
+            parameters[key].update(value.variables)
+        cls.__arguments__ = arguments
+        cls.__parameters__ = parameters
 
     def __call__(cls, *args, **kwargs):
         pass
