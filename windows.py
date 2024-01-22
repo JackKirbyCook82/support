38c8b5abe4981006f7a0fb257cbb5e134715aa42
JackKirbyCook82 <jackcookchee@yahoo.com>
2024-01-21 18:12:26 -0600

    Updated 1/10/2024

--- windows.py
+++ windows.py
@@ -14,11 +14,12 @@ from collections import namedtuple as ntuple
 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
-__all__ = ["Windows", "Window", "Frame", "Table", "Button", "Text", "Column", "Justify"]
+__all__ = ["Window", "Frame", "Table", "Button", "Text", "Column", "Justify"]
 __copyright__ = "Copyright 2022, Jack Kirby Cook"
 __license__ = ""
 
 
+Trinket = IntEnum("Trinket", ["WINDOW", "FRAME", "BUTTON", "TABLE"], start=1)
 Justify = IntEnum("Justify", ["LEFT", "CENTER", "RIGHT"], start=1)
 Column = ntuple("Column", "name width parser")
 Text = ntuple("Format", "name font parser")
@@ -27,149 +28,123 @@ Text = ntuple("Format", "name font parser")
 class ElementMeta(ABCMeta):
     def __new__(mcs, name, bases, attrs, *args, **kwargs):
         attrs = {key: value for key, value in attrs.items() if not isinstance(value, Text) or not isinstance(value, Column)}
-        return super(ElementMeta, mcs).__new__(mcs, name, bases, attrs)
+        return super(ElementMeta, mcs).__new__(mcs, name, bases, attrs, *args, **kwargs)
 
-    def __init__(cls, *args, **kwargs): pass
-    def __call__(cls, *args, **kwargs):
-        instance = super(ElementMeta, cls).__call__(*args, **cls.parameters, **kwargs)
-        return instance
-
-    @property
-    def parameters(cls): return {}
-
-
-class ActionMeta(ElementMeta):
-    def __init__(cls, *args, **kwargs):
-        super(ActionMeta, cls).__init__(*args, **kwargs)
-        cls.__actions__ = {key: value for key, value in getattr(cls, "__actions__", {}).items()}
-
-    def __call__(cls, *args, **kwargs):
-        actions = {key: value for key, value in cls.__actions__.items()}
-        actions = [actions[priority] for priority in range(len(actions))]
-        instance = super(ActionMeta, cls).__call__(*args, actions, **kwargs)
-        return instance
-
-    def register(cls, priority=1):
-        assert isinstance(priority, int) and priority >= 0
-        assert priority not in cls.__actions__.keys()
-
-        def decorator(method):
-            cls.__actions__[priority] = method
-            return method
-        return decorator
-
-
-class FrameMeta(ElementMeta):
     def __init__(cls, name, bases, attrs, *args, **kwargs):
-        super(FrameMeta, cls).__init__(name, bases, attrs, *args, **kwargs)
+        columns = {key: value for key, value in attrs.items() if isinstance(value, Column)}
         texts = {key: value for key, value in attrs.items() if isinstance(value, Text)}
+        trinket = kwargs.get("trinket", getattr(cls, "__trinket__", None))
+        columns = getattr(cls, "__columns__", {}) | columns
         texts = getattr(cls, "__texts__", {}) | texts
+        cls.__trinket__ = trinket
+        cls.__columns__ = columns
         cls.__texts__ = texts
 
-    @property
-    def parameters(cls): return dict(texts=cls.__texts__)
-
-
-class ButtonMeta(ActionMeta): pass
-class TableMeta(ActionMeta):
-    def __init__(cls, name, bases, attrs, *args, **kwargs):
-        super(TableMeta, cls).__init__(name, bases, attrs, *args, **kwargs)
-        columns = {key: value for key, value in attrs.items() if isinstance(value, Column)}
-        justify = kwargs.get("justify", getattr(cls, "__justify__", Justify.RIGHT))
-        height = kwargs.get("height", getattr(cls, "__height__", None))
-        events = kwargs.get("events", getattr(cls, "__events__", False))
-        columns = getattr(cls, "__columns__", {}) | columns
-        assert isinstance(height, (int, type(None))) and isinstance(events, bool)
-        cls.__columns__ = columns
-        cls.__justify__ = justify
-        cls.__height__ = height
-        cls.__events__ = events
-
-    @property
-    def parameters(cls):
-        header = [str(value.name) for value in cls.__columns__.values()]
-        width = [int(value.width) for value in cls.__columns__.values()]
-        justify = str(cls.__justify__.name).lower()
-        formatting = dict(col_widths=width, row_height=cls.__height__, auto_size_columns=False, justification=justify)
-        return dict(columns=cls.__columns__, header=header, formatting=formatting, events=cls.__events__)
+    def __call__(cls, *args, **kwargs):
+        print(kwargs)
+        parameters = dict(trinket=cls.__trinket__, columns=cls.__columns__, texts=cls.__texts__)
+        instance = super(ElementMeta, cls).__call__(*args, **parameters, **kwargs)
+        return instance
 
 
 class Element(ABC, metaclass=ElementMeta):
+    def __str__(self): return self.key(name=self.name, index=self.index, trinket=self.trinket)
     def __repr__(self): return self.name
-    def __init__(self, *args, name, element, **kwargs):
+
+    def __init_subclass__(cls, *args, **kwargs): pass
+    def __init__(self, *args, name, index, trinket, element, **kwargs):
         self.__element = element
+        self.__trinket = trinket
+        self.__index = index
         self.__name = name
+
+    @staticmethod
+    def key(*args, name, index, trinket, **kwargs):
+        return f"{str(name).lower()}|{int(index):.0f}|{str(trinket.name).lower()}"
 
     @property
     def element(self): return self.__element
     @property
+    def trinket(self): return self.__trinket
+    @property
+    def index(self): return self.__index
+    @property
     def name(self): return self.__name
 
 
-class Frame(Element, ABC, metaclass=FrameMeta):
-    def __init__(self, *args, name, key, content, texts={}, **kwargs):
+class Button(Element, ABC, trinket=Trinket.BUTTON):
+    def __init__(self, *args, name, **kwargs):
+        key = self.key(*args, name=name, **kwargs)
+        element = gui.Button(name, key=key, metadata=self)
+        super().__init__(*args, name=name, element=element, **kwargs)
+
+
+class Frame(Element, ABC, trinket=Trinket.FRAME):
+    def __init__(self, *args, name, content, texts={}, **kwargs):
         create = lambda strings, font: [gui.Text(string, font=font) for string in strings] if isinstance(strings, list) else gui.Text(strings, font=font)
         texts = ODict([(value.name, create(value.parser(content), value.font)) for value in texts.values()])
         layout = self.layout(*args, **texts, **kwargs)
-        element = gui.Frame("", layout, key=key)
-        super().__init__(*args, name=name, key=key, element=element, **kwargs)
+        element = gui.Frame("", layout)
+        super().__init__(*args, name=name, element=element, **kwargs)
 
     @staticmethod
     @abstractmethod
     def layout(*args, **kwargs): pass
 
 
-class Button(Element, ABC, metaclass=ButtonMeta):
-    def __init__(self, *args, name, key, clicking=[], **kwargs):
-        element = gui.Button(name, key=key)
-        super().__init__(*args, name=name, element=element, **kwargs)
-        self.__clicking = clicking
+class Table(Element, ABC, trinket=Trinket.TABLE):
+    def __init_subclass__(cls, *args, **kwargs):
+        super().__init_subclass__(*args, **kwargs)
+        justify = kwargs.get("justify", getattr(cls, "__justify__", Justify.RIGHT))
+        height = kwargs.get("height", getattr(cls, "__height__", None))
+        events = kwargs.get("events", getattr(cls, "__events__", False))
+        assert isinstance(height, (int, type(None))) and isinstance(events, bool)
+        cls.__justify__ = justify
+        cls.__height__ = height
+        cls.__events__ = events
 
-
-class Table(Element, ABC, metaclass=TableMeta):
-    def __init__(self, *args, name, key, content=[], columns={}, header, formatting, events, selecting, **kwargs):
+    def __init__(self, *args, name, content=[], columns={}, **kwargs):
+        header = [str(value.name) for value in columns.values()]
+        width = [int(value.width) for value in columns.values()]
+        justify = str(self.__class__.__justify__.name).lower()
+        height = int(self.__class__.__height__)
+        events = bool(self.__class__.__events__)
         columns = ODict([(value.name, value.parser) for value in columns.values()])
+        formatting = dict(col_widths=width, row_height=height, auto_size_columns=False, justification=justify)
+        parameters = dict(headings=header, enable_events=events)
         layout = [[parser(row) for name, parser in columns.items()] for row in content]
-        element = gui.Table(layout, key=key, headings=header, enable_events=events, **formatting)
-        super().__init__(*args, name=name, key=key, element=element, **kwargs)
+        key = self.key(*args, name=name, **kwargs)
+        element = gui.Table(layout, key=key, metadata=self, **formatting, **parameters)
+        super().__init__(*args, name=name, element=element, **kwargs)
         self.__columns = columns
-        self.__selecting = selecting
-
-    def update(self, *args, content=[], **kwargs):
-        layout = [[parser(row) for name, parser in self.columns.items()] for row in content]
-        self.element.update(layout)
 
     @property
     def columns(self): return self.__columns
 
 
-class WindowMeta(ABCMeta):
-    def __call__(cls, *args, **kwargs):
-        instance = super(WindowMeta, cls).__call__(*args, **kwargs)
-        instance.start()
-        return instance
-
-
-class Window(ABC, metaclass=WindowMeta):
+class Window(Element, trinket=Trinket.WINDOW):
     def __bool__(self): return self.opened and not self.closed
-    def __repr__(self): return self.name
-    def __str__(self): return self.key
-    def __init__(self, *args, name, key, **kwargs):
+    def __init__(self, *args, name, **kwargs):
         layout = self.layout(*args, **kwargs)
-        window = gui.Window(name, layout, resizable=True, finalize=False, metadata=key)
+        element = gui.Window(name, layout, resizable=True, finalize=False, metadata=self)
+        super().__init__(*args, name=name, element=element, **kwargs)
         self.__opened = False
         self.__closed = False
-        self.__window = window
-        self.__name = name
-        self.__key = key
 
-    def start(self):
-        self.window.finalize()
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, error_type, error_value, error_traceback):
+        self.stop()
+
+    def start(self, *args, **kwargs):
+        self.element.finalize()
         self.closed = False
         self.opened = True
 
-    def stop(self):
-        self.window.close()
+    def stop(self, *args, **kwargs):
+        self.element.close()
         self.closed = True
         self.opened = False
 
@@ -182,28 +157,9 @@ class Window(ABC, metaclass=WindowMeta):
     @closed.setter
     def closed(self, closed): self.__closed = closed
     @property
-    def opened(self): return self.__started
+    def opened(self): return self.__opened
     @opened.setter
     def opened(self, opened): self.__opened = opened
-    @property
-    def window(self): return self.__window
-    @property
-    def name(self): return self.__name
-    @property
-    def key(self): return self.__key
-
-
-class Windows(dict):
-    def __bool__(self): return any([bool(window) for window in self.values()])
-    def __enter__(self): return self
-    def __exit__(self, error_type, error_value, error_traceback):
-        for window in self.values():
-            window.stop()
-
-
-
-
-
 
 
 
