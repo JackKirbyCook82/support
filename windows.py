bab6b26269e7a2bc1352c393bb98aad979c9cb1d
JackKirbyCook82 <jackcookchee@yahoo.com>
2024-07-18 22:04:39 -0500

    Updated 1/10/2024

---
+++ windows.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thurs Jul 18 2024
+@name:   Window Objects
+@author: Jack Kirby Cook
+
+"""
+
+import logging
+
+__version__ = "1.0.0"
+__author__ = "Jack Kirby Cook"
+__all__ = []
+__copyright__ = "Copyright 2023, Jack Kirby Cook"
+__license__ = "MIT License"
+__logger__ = logging.getLogger(__name__)
+
