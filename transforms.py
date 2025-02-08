26fd26f3ad9097faf5f5e84edd614c3edf68a18c
JackKirbyCook82 <jackcookchee@yahoo.com>
2025-02-07 19:16:02 -0600

    Updated 1/10/2024

--- transforms.py
+++ transforms.py
@@ -13,7 +13,7 @@ from support.mixins import Sizing, Emptying, Partition, Logging
 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
-__all__ = ["Pivot", "Unpivot"]
+__all__ = ["Pivoter", "Unpivoter"]
 __copyright__ = "Copyright 2023, Jack Kirby Cook"
 __license__ = "MIT License"
 
@@ -25,9 +25,7 @@ class Transform(Sizing, Emptying, Partition, Logging, ABC, title="Transformed"):
 
     def __init__(self, *args, header, **kwargs):
         super().__init__(*args, **kwargs)
-        index, columns = list(header)
-        self.__columns = columns
-        self.__index = index
+        self.__header = header
 
     def execute(self, dataframes, *args, **kwargs):
         assert isinstance(dataframes, pd.DataFrame)
@@ -48,28 +46,28 @@ class Transform(Sizing, Emptying, Partition, Logging, ABC, title="Transformed"):
     @property
     def query(self): return type(self).__query__
     @property
-    def columns(self): return self.__columns
-    @property
-    def index(self): return self.__index
+    def header(self): return self.__header
 
 
-class Pivot(Transform, title="Pivoted"):
+class Pivoter(Transform, title="Pivoted"):
     def calculate(self, dataframe, *args, **kwargs):
         assert isinstance(dataframe, pd.DataFrame)
-        index = set(dataframe.columns) - ({self.index} | set(self.columns))
-        dataframe = dataframe.pivot(index=list(index), columns=[self.index])
+        by, values = self.header.pivot
+        index = set(dataframe.columns) - ({by} | set(values))
+        dataframe = dataframe.pivot(index=list(index), columns=[by])
         dataframe = dataframe.reset_index(drop=False, inplace=False)
         return dataframe
 
 
-class Unpivot(Transform, title="Unpivoted"):
+class Unpivoter(Transform, title="Unpivoted"):
     def calculate(self, dataframe, *args, **kwargs):
         assert isinstance(dataframe, pd.DataFrame)
+        by, values = self.header.unpivot
         dataframe.index.name = "index"
-        level = list(dataframe.columns.names).index(self.index)
-        index = set(dataframe.columns) - set(self.columns)
+        level = list(dataframe.columns.names).index(by)
+        index = set(dataframe.columns) - set(values)
         columns = set([values for values in dataframe.columns.values if bool(values[level])])
-        index = dataframe[list(index)].stack().reset_index(drop=False, inplace=False).drop(columns=self.index)
+        index = dataframe[list(index)].stack().reset_index(drop=False, inplace=False).drop(columns=by)
         columns = dataframe[list(columns)].stack().reset_index(drop=False, inplace=False)
         dataframe = pd.merge(index, columns, how="outer", on="index").drop(columns="index")
         return dataframe
