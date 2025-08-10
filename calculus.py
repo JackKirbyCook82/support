92b5b10e0dac5dfbf695da35233000a068d91e8f
JackKirbyCook82 <jackcookchee@yahoo.com>
2025-08-10 17:14:46 -0500

    Updated 1/10/2024

--- calculus.py
+++ calculus.py
@@ -10,9 +10,13 @@ from abc import ABC
 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
-__all__ = ["Integral"]
+__all__ = ["Function", "Integral"]
 __copyright__ = "Copyright 2023, Jack Kirby Cook"
 __license__ = "MIT License"
+
+
+class Function(ABC):
+    pass
 
 
 class Integral(ABC):
