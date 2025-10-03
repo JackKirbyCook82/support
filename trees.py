f2005e7af3bad67154e3b4f22bff63469fd2121b
JackKirbyCook82 <jackcookchee@yahoo.com>
2025-10-03 04:31:12 -0500

    Updated 1/10/2024

--- trees.py
+++ trees.py
@@ -50,6 +50,7 @@ class Node(Mixin):
     def __getitem__(self, key): return self.get(key)
     def __reversed__(self): return reversed(self.items())
     def __iter__(self): return iter(self.items())
+    def __str__(self): return self.render()
 
     def keys(self): return self.children.keys()
     def values(self): return self.children.values()
