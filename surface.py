a6699a88a8c504d7d08dc4e4ab40abbea9b1b6c8
JackKirbyCook82 <jackcookchee@yahoo.com>
2026-04-22 02:31:52 -0500

    Updated 1/10/2024

--- surface.py
+++ surface.py
@@ -8,15 +8,16 @@ Created on Tues Apr 21 2026
 
 import numpy as np
 import pandas as pd
+from enum import Enum
 from abc import ABC, abstractmethod
 from collections import namedtuple as ntuple
 from scipy.interpolate import UnivariateSpline, CubicSpline, PchipInterpolator, Akima1DInterpolator, RectBivariateSpline, SmoothBivariateSpline
 
-from support.concepts import NumRange
+from support.concepts import NumRange, Assembly
 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
-__all__ = []
+__all__ = ["Curves", "Surfaces"]
 __copyright__ = "Copyright 2026, Jack Kirby Cook"
 __license__ = "MIT License"
 
@@ -25,17 +26,19 @@ CurveAxes = ntuple("Axes", "x y")
 SurfaceAxes = ntuple("Axes", "x y z")
 
 
+class Curvature(Enum): REGRESSIVE, INTERPOLATIVE, SHAPE, VISUAL = range(4)
 class Curve(ABC):
     def __call__(self, yaxis): return self.curve(yaxis)
     def __init__(self, yaxis, zaxis, /, **kwargs):
-        assert all([isinstance(axis, (pd.Series, np.ndarray)) for axis in (yaxis, zaxis)])
-        assert len(yaxis) == len(zaxis)
         yaxis = np.asarray(yaxis, dtype=np.float32)
         zaxis = np.asarray(zaxis, dtype=np.float32)
+        assert yaxis.ndim == zaxis.ndim == 1
+        assert len(yaxis) == len(zaxis)
         order = np.argsort(yaxis)
+        assert not np.any(np.diff(yaxis) <= 0)
         yaxis, zaxis = (yaxis[order], zaxis[order])
         curve = self.create(yaxis, zaxis, **kwargs)
-        boundary = NumRange.create([yaxis.min(), zaxis.max()])
+        boundary = NumRange.create([yaxis.min(), yaxis.max()])
         self.__boundary = boundary
         self.__curve = curve
 
@@ -81,7 +84,7 @@ class Surface(ABC):
     def __call__(self, x, y):
         x, y = np.asarray(x, dtype=float), np.asarray(y, dtype=float)
         assert not self.violation(x, y)
-        return self.surface(x, x)
+        return self.surface(x, y)
 
     def z(self, x, y):
         x, y = np.asarray(x, dtype=float), np.asarray(y, dtype=float)
@@ -115,7 +118,7 @@ class Surface(ABC):
 
     def violation(self, x, y):
         boundary = self.boundary()
-        x = np.any((x < boundary.x.minimum) | (y > boundary.t.maximum))
+        x = np.any((x < boundary.x.minimum) | (x > boundary.x.maximum))
         y = np.any((y < boundary.y.minimum) | (y > boundary.y.maximum))
         return x | y
 
@@ -131,7 +134,7 @@ class Surface(ABC):
 class RegressiveSurface(Surface):
     def create(self, xaxis, yaxis, zaxis, /, **kwargs):
         surface = self.regression(xaxis, yaxis, zaxis, **kwargs)
-        domain = self.footprint(xaxis, yaxis, **kwargs)
+        domain = self.grid(xaxis, yaxis, **kwargs)
         return surface, domain
 
     @staticmethod
@@ -139,55 +142,58 @@ class RegressiveSurface(Surface):
         return SmoothBivariateSpline(xaxis, yaxis, zaxis, w=weights, kx=degree.x, ky=degree.y, s=smoothing)
 
     @staticmethod
-    def footprint(xaxis, yaxis, /, grid, **kwargs):
+    def grid(xaxis, yaxis, /, grid, **kwargs):
         xaxis = np.linspace(xaxis.min(), xaxis.max(), grid)
         yaxis = np.linspace(yaxis.min(), yaxis.max(), grid)
         return CurveAxes(xaxis, yaxis)
 
 
 class InterpolativeSurface(Surface):
-    def create(self, xaxis, yaxis, zaxis, /, **kwargs):
-        dataframe = pd.concat([xaxis, yaxis, zaxis], axis=1)
+    def create(self, xaxis, yaxis, zaxis, /, curvature, **kwargs):
+        dataframe = pd.DataFrame({"x": xaxis, "y": yaxis, "z": zaxis})
         dataframe = dataframe.groupby(["x", "y"], as_index=False)["z"].mean()
         samples = self.samples(dataframe, **kwargs)
-        curves = [(xaxis, RegressiveCurve(yaxis, zaxis, **kwargs)) for (xaxis, yaxis, zaxis) in samples]
-        surface = self.surface(curves, **kwargs)
-        domain = self.domain(curves, **kwargs)
+        curves = {Curvature.REGRESSIVE: RegressiveCurve, Curvature.INTERPOLATIVE: InterpolativeCurve, Curvature.SHAPE: ShapeInterpolativeCurve, Curvature.VISUAL: VisualInterpolativeCurve}
+        curves = [(xaxis, curves[curvature](yaxis, zaxis, **kwargs)) for (xaxis, yaxis, zaxis) in samples]
+        surface = self.interpolation(curves, **kwargs)
+        domain = self.grid(curves, **kwargs)
         return surface, domain
 
     @staticmethod
     def samples(dataframe, /, size, **kwargs):
         for xaxis, sample in dataframe.groupby("x", sort="x"):
-            sample = sample.sort("x")
+            sample = sample.sort_values("y")
             yaxis = sample["y"].to_numpy()
             zaxis = sample["z"].to_numpy()
             if len(yaxis) < size: continue
-            if np.any(np.diff(yaxis) > 0): continue
-            order = np.argsort(yaxis)
-            sample = SurfaceAxes(xaxis, yaxis[order], zaxis[order])
+            if np.any(np.diff(yaxis) <= 0): continue
+            sample = SurfaceAxes(xaxis, yaxis, zaxis)
             yield sample
 
     @staticmethod
     def interpolation(curves, /, degree, smoothing, grid, **kwargs):
-        left = max(curve.boundary.minimum for xaxis, curve in curves)
-        right = min(curve.boundary.maximum for xaxis, curve in curves)
+        left = max(curve.boundary.minimum for (xaxis, curve) in curves)
+        right = min(curve.boundary.maximum for (xaxis, curve) in curves)
         assert left < right
-        xaxis = np.array([curve.sample.t for curve in curves])
+        xaxis = np.array([xaxis for (xaxis, curve) in curves], dtype=float)
         yaxis = np.linspace(left, right, grid)
-        zaxis = np.array([curve.spline(yaxis) for index, curve in enumerate(curves)])
+        zaxis = np.array([curve(yaxis) for (xaxis, curve) in curves], dtype=float)
         return RectBivariateSpline(xaxis, yaxis, zaxis, kx=degree.x, ky=degree.y, s=smoothing)
 
     @staticmethod
-    def footprint(curves, /, grid, **kwargs):
-        left = max(curve.boundary.minimum for xaxis, curve in curves)
-        right = min(curve.boundary.maximum for xaxis, curve in curves)
+    def grid(curves, /, grid, **kwargs):
+        left = max(curve.boundary.minimum for (xaxis, curve) in curves)
+        right = min(curve.boundary.maximum for (xaxis, curve) in curves)
         assert left < right
-        xaxis = np.array([xaxis for (xaxis, curve) in curves])
+        xaxis = np.array([xaxis for (xaxis, curve) in curves], dtype=float)
         yaxis = np.linspace(left, right, grid)
         return CurveAxes(xaxis, yaxis)
 
 
-
+class Surfaces(Assembly): Regressive, Interpolative = RegressiveSurface, InterpolativeSurface
+class Curves(Assembly):
+    class Regressive(Assembly): Standard = RegressiveCurve
+    class Interpolative(Assembly): Standard, Shape, Visual = InterpolativeCurve, ShapeInterpolativeCurve, VisualInterpolativeCurve
 
 
 
