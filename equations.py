0b07a0428f9079cc534586a59083317f2b1ac7dc
JackKirbyCook82 <jackcookchee@yahoo.com>
2026-06-21 02:38:45 -0500

    Updated 1/10/2024

--- equations.py
+++ equations.py
@@ -9,6 +9,7 @@ Created on Tues Apr 14 2026
 import types
 import inspect
 import pandas as pd
+from typing import Callable
 from dataclasses import dataclass
 from graphlib import TopologicalSorter, CycleError
 
@@ -22,13 +23,9 @@ __copyright__ = "Copyright 2026, Jack Kirby Cook"
 __license__ = "MIT License"
 
 
-class Missing: __slots__ = ()
-MISSING = Missing()
-
-
 @dataclass(frozen=True)
 class Equation:
-    variable: str; function: callable; arguments: tuple[str, ...]; parameters: tuple[str, ...]
+    variable: str; function: Callable; arguments: tuple[str, ...]; parameters: tuple[str, ...]
 
     @classmethod
     def create(cls, variable, function):
@@ -60,22 +57,21 @@ class EquationsMeta(Meta):
         updated = list(kwargs.get("variables", updated.keys()))
         inherited = [variable for base in bases for variable in getattr(base, "__variables__", [])]
         variables = list(dict.fromkeys(inherited + updated))
-        updated = kwargs.get("defaults", {})
-        inherited = {key: value for base in bases for key, value in getattr(base, "__defaults__", {}).items()}
-        defaults = inherited | updated
+        updated = kwargs.get("parameters", {})
+        inherited = {parameter for base in bases for parameter, default in getattr(base, "__hyperparams__", {}).items()}
+        hyperparams = inherited | updated
+        cls.__hyperparams__ = hyperparams
         cls.__functions__ = functions
         cls.__variables__ = variables
-        cls.__defaults__ = defaults
 
     def __call__(cls, *args, **kwargs):
         signatures = [inspect.signature(function).parameters.items() for function in cls.functions.values()]
         arguments = [variable for signature in signatures for variable, details in signature if details.kind in (details.POSITIONAL_OR_KEYWORD, details.POSITIONAL_OR_KEYWORD)]
-        parameters = {variable for signature in signatures for variable, details in signature if details.kind == details.KEYWORD_ONLY}
         dependents = {argument for argument in arguments if argument in cls.functions.keys()}
         independents = {argument for argument in arguments if argument not in cls.functions.keys()}
         equations = {variable: Equation.create(variable, function) for variable, function in cls.functions.items()}
-        hyperparams = {parameter: kwargs.get(parameter, cls.defaults.get(parameter, MISSING)) for parameter in parameters}
-        instance = super().__call__(*args, equations=equations, hyperparams=hyperparams, independents=independents, dependents=dependents, **kwargs)
+        parameters = {variable: kwargs.get(variable, default) for variable, default in cls.hyperparams.items()}
+        instance = super().__call__(*args, equations=equations, independents=independents, dependents=dependents, parameters=parameters, **kwargs)
         return instance
 
     @property
@@ -83,28 +79,26 @@ class EquationsMeta(Meta):
     @property
     def variables(cls): return cls.__variables__
     @property
-    def defaults(cls): return cls.__defaults__
+    def hyperparams(cls): return cls.__hyperparams__
 
 
 class Equations(Mixin, metaclass=EquationsMeta):
-    def __init__(self, *args, equations, hyperparams, dependents, independents, **kwargs):
+    def __getitem__(self, equation): return self.equations[equation]
+    def __init__(self, *args, equations, dependents, independents, parameters, **kwargs):
         super().__init__(*args, **kwargs)
-        self.__hyperparams = hyperparams
+        self.__parameters = parameters
         self.__independents = independents
         self.__dependents = dependents
         self.__equations = equations
 
     def execute(self, dataframe, *args, **kwargs):
         assert isinstance(dataframe, pd.DataFrame)
-        missing = {argument for argument in self.independents if argument not in dataframe.columns}
+        missing = {argument for argument in self.independents if argument not in dataframe.columns and argument not in kwargs.keys()}
         if bool(missing): raise EquationArgumentError()
-        keywords = {parameter: kwargs.get(parameter, default) for parameter, default in self.hyperparams.items()}
-        if MISSING in keywords.values(): raise EquationParameterError()
         dataframe = dataframe.copy()
         for equation in self.order:
             arguments = [dataframe[argument] for argument in equation.arguments]
-            parameters = {parameter: keywords[parameter] for parameter in equation.parameters}
-            dataframe[equation.variable] = equation.function(*arguments, **parameters)
+            dataframe[equation.variable] = equation.function(*arguments, **self.parameters)
         if not bool(type(self).variables): return dataframe
         columns = list(type(self).variables)
         return dataframe[columns]
@@ -117,13 +111,12 @@ class Equations(Mixin, metaclass=EquationsMeta):
         return [self.equations[variable] for variable in order if variable in dependency]
 
     @property
-    def hyperparams(self): return self.__hyperparams
-    @property
     def independents(self): return self.__independents
     @property
     def dependents(self): return self.__dependents
     @property
     def equations(self): return self.__equations
-
+    @property
+    def parameters(self): return self.__parameters
 
 
