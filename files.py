9e98d87cf5b5378a0994b21b06ff1b19988770b0
JackKirbyCook82 <jackcookchee@yahoo.com>
2026-08-19 18:07:19 -0500

    Updated 1/10/2024

--- files.py
+++ files.py
@@ -8,7 +8,11 @@ Created on Weds Aug 5 2026
 
 import multiprocessing
 import pandas as pd
+from pathlib import Path
 from dataclasses import dataclass
+from abc import ABC, abstractmethod
+
+from support.mixins import Logging
 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
@@ -27,61 +31,69 @@ class Header:
         self.parsers = {key: value for key, value in self.parsers.items() if key in self.columns}
 
 
-class FileMeta(type):
+class File(Logging, ABC):
     locking = {}
 
-    def __init__(cls, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        header = kwargs.get("header", getattr(cls, "__header__", None))
+    def __init_subclass__(cls, header, **kwargs):
+        super().__init_subclass__(**kwargs)
+        assert isinstance(header, Header)
         cls.__header__ = header
 
-    def __call__(cls, *args, file, **kwargs):
-        mutex = FileMeta.locking.get(file, multiprocessing.Lock())
-        instance = super().__call__(*args, file=file, mutex=mutex, **kwargs)
+    def __new__(cls, *args, file, **kwargs):
+        mutex = File.locking.get(file, multiprocessing.Lock())
+        File.locking[file] = mutex
+        instance = super().__new__(cls, *args, **kwargs)
+        instance.mutex = mutex
         return instance
 
-    @property
-    def header(cls): return cls.__header__
-
-
-class File(object, metaclass=FileMeta):
-    def __init__(self, *args, file, mutex, **kwargs):
-        assert isinstance(type(self).header, Header)
-        self.__mutex = mutex
+    def __init__(self, *args, file, **kwargs):
+        assert isinstance(file, Path)
+        super().__init__(*args, **kwargs)
+        self.__mutex = None
         self.__file = file
 
     def save(self, dataframe, mode):
         assert isinstance(dataframe, pd.DataFrame)
         assert isinstance(mode, str) and mode in ("w", "a")
         self.file.parent.mkdir(exist_ok=True, parents=True)
-        dataframe = dataframe[type(self).header.columns].copy()
-        for column, formatter in type(self).header.formatting.items():
+        dataframe = dataframe[self.header.columns].copy()
+        for column, formatter in self.header.formatting.items():
             dataframe[column] = dataframe[column].apply(formatter)
-        for column, astype in type(self).header.typing.items():
+        for column, astype in self.header.typing.items():
             dataframe[column] = dataframe[column].apply(astype)
         with self.mutex:
             dataframe.to_csv(self.file, mode=mode, float_format="%.3f", index=False)
+        self.results(dataframe, title="Saved")
 
     def load(self, mode="r"):
         assert isinstance(mode, str) and mode == "r"
         if not self.file.exists():
-            mapping = type(self).header.typing.items()
+            mapping = self.header.typing.items()
             mapping = {column: pd.Series(dtype=astype) for column, astype in mapping}
             dataframe = pd.DataFrame(mapping)
             return dataframe
-        else:
-            with self.mutex:
-                dataframe = pd.read_csv(self.file)
-            for column, astype in type(self).header.typing.items():
-                dataframe[column] = dataframe[column].apply(astype)
-            for column, parser in type(self).header.parsers.items():
-                dataframe[column] = dataframe[column].apply(parser)
-            return dataframe
+        with self.mutex:
+            dataframe = pd.read_csv(self.file)
+        for column, astype in self.header.typing.items():
+            dataframe[column] = dataframe[column].apply(astype)
+        for column, parser in self.header.parsers.items():
+            dataframe[column] = dataframe[column].apply(parser)
+        self.results(dataframe, title="Loaded")
+        return dataframe
+
+    @abstractmethod
+    def results(self, dataframe, *args, title, **kwargs): pass
+
+    @property
+    def header(self): return type(self).__header__
+    @property
+    def file(self): return self.__file
 
     @property
     def mutex(self): return self.__mutex
-    @property
-    def file(self): return self.__file
+    @mutex.setter
+    def mutex(self, mutex): self.__mutex = mutex
+
 
 
 
